36e97824c0ef9e0dc5054fcdd8bf81982e2d4821
Emival Santos <emivalvictor@hotmail.com>
2021-04-21 18:45:48 -0300

    Feat: Change blog name and add profile picture

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -3,8 +3,8 @@ import { HeaderContainer, Logo, Title } from "./styles/header.styles";
 export default function Header(children, ...props) {
   return (
     <HeaderContainer>
-      <Logo src="images/logo.svg"></Logo>
-      <Title>BATATA BLOG</Title>
+      <Logo src="images/profile.jfif"></Logo>
+      <Title>Emival Santos</Title>
     </HeaderContainer>
   );
 }
--- src/components/header/styles/header.styles.js
+++ src/components/header/styles/header.styles.js
@@ -2,18 +2,20 @@ import styled from "styled-components";
 
 export const HeaderContainer = styled.div`
   display: flex;
-  justify-content: space-between;
-  align-items: center;
-  border-bottom: 1px solid white;
-  background: lightgray;
+  justify-content: flex-start;
+  align-items: flex-end;
+  background: black;
 `;
 
 export const Logo = styled.img`
-  height: 2rem;
+  height: 5rem;
+  border-radius: 50%;
+  margin-left: 1rem;
 `;
 
 export const Title = styled.h1`
   font-size: 1.25rem;
-  margin-right: 1rem;
-  color: black;
+  margin-left: 1rem;
+  padding: 1rem 0;
+  color: white;
 `;
