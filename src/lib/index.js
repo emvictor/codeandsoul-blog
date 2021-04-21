a8b12dcdc66401cba91d046fcae83504f53d5e10
Emival Santos <emivalvictor@hotmail.com>
2021-04-21 19:29:46 -0300

    Fix: post title link lead to next post in the list

--- src/lib/index.js
+++ src/lib/index.js
@@ -14,7 +14,7 @@ export async function getPostsID() {
 
 export async function getPostData(postID) {
   const posts = await getPostsData();
-  const { userId, id, title, body } = posts[postID];
+  const { userId, id, title, body } = posts[postID - 1];
   return {
     userId,
     id,
