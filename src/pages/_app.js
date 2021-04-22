570fada5a9135e1d7e3777aa7be6af987985b396
Emival Santos <emivalvictor@hotmail.com>
2021-04-22 13:38:21 -0300

    Fix: meta tags with next

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,7 +1,16 @@
+import Head from "next/head";
 import { GlobalStyles } from "../styles/globalstyles";
 function MyApp({ Component, pageProps }) {
   return (
     <>
+      <Head>
+        <title>Code and Soul Blog | Next.js</title>
+        <meta
+          name="description"
+          content="Blog example using next.js SSG and external API"
+        />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      </Head>
       <GlobalStyles />
       <Component {...pageProps} />
     </>
--- src/pages/_document.js
+++ src/pages/_document.js
@@ -10,18 +10,9 @@ class MyDocument extends Document {
     return (
       <Html lang="en">
         <Head>
-          <title>Code and Soul Blog | Next.js</title>
-          <meta
-            name="description"
-            content="Blog example using next.js SSG and external API"
-          />
-          <meta http-equiv="Content-Type" content="text/html" />
+          <meta httpEquiv="Content-Type" content="text/html" />
           <meta charSet="utf-8" />
           <link rel="icon" href="./favicon.svg" />
-          <meta
-            name="viewport"
-            content="width=device-width, initial-scale=1.0"
-          />
         </Head>
         <body>
           <Main />
