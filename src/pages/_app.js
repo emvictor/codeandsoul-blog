4be7404b949b4c564552c854d2c398e2aace1180
Emival Santos <emivalvictor@hotmail.com>
2021-04-22 14:00:03 -0300

    Fix: remove Head

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -3,16 +3,6 @@ import { GlobalStyles } from "../styles/globalstyles";
 function MyApp({ Component, pageProps }) {
   return (
     <>
-      <Head>
-        <title>Code and Soul Blog | Next.js</title>
-        <meta
-          name="description"
-          content="Blog example using next.js SSG and external API"
-        />
-        <meta charSet="utf-8" />
-        <link rel="icon" href="./favicon.svg" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-      </Head>
       <GlobalStyles />
       <Component {...pageProps} />
     </>
