53cfacd6355f161de781b9df1a17bead1dd3c390
Emival Santos <emivalvictor@hotmail.com>
2021-04-21 22:09:13 -0300

    Chore: Add Read More components

---
+++ src/components/read-more/index.js
@@ -0,0 +1,22 @@
+import {
+  Container,
+  Text,
+  Post,
+  Inner,
+} from "../../styles/components/readmore.styles";
+
+export default function ReadMore({ children, ...props }) {
+  return <Container {...props}>{children}</Container>;
+}
+
+ReadMore.Text = function ReadMoreText({ children, ...props }) {
+  return <Text {...props}>{children}</Text>;
+};
+
+ReadMore.Post = function ReadMorePost({ children, ...props }) {
+  return <Post {...props}>{children}</Post>;
+};
+
+ReadMore.Inner = function ReadMoreInner({ children, ...props }) {
+  return <Inner {...props}>{children}</Inner>;
+};
